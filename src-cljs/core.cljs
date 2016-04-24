d92f07ce3e61f708efae2179b22f07f8fa92e9ff
Torsten Scholz <doaschdn@googlemail.com>
2016-04-24 17:39:44 +0200

    Add possibility to add new regex entries

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -1,16 +1,41 @@
 (ns parva-pila-rex.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [cljs.core.async :refer [<! >!]]
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]))
 
 (defn append-li [content]
-  (ef/append
-    (ef/html [:li {:class "list-group-item"} [:code content]])))
+  (when (:name content)
+    (ef/append
+      (ef/html [:li
+                 {:class "list-group-item"}
+                 [:h3 (:name content)]
+                 [:p {:class "small"} (:desc content)]
+                 [:code (:regex content)]]))))
+
+(defn update-regex-list [regexes]
+  (ef/at "#regex-list" (ef/content nil))
+  (doseq [regex regexes]
+    (ef/at "#regex-list" (append-li regex))))
 
 (defn ^:export init []
   (go
     (let [response (<! (http/get "/regexes"))
           regexes (:body response)]
-      (doseq [regex regexes]
-        (ef/at "#regex-list" (append-li (:regex regex)))))))
+      (update-regex-list regexes)
+      (ef/at "#btn-create-regex" (ev/listen
+                                   :click
+                                   #(go (update-regex-list (:body (<! (http/post "/regexes"
+                                      {:json-params
+                                        {:name (ef/from "#input-regex-name" (ef/get-prop :value))
+                                         :desc (ef/from "#input-regex-desc" (ef/get-prop :value))
+                                         :regex (ef/from "#input-regex" (ef/get-prop :value))}})))))))
+
+      (ef/at "#btn-delete-regex" (ev/listen
+                                  :click
+                                  #(go (update-regex-list (:body (<! (http/delete "/regexes"
+                                     {:json-params
+                                       {:name (ef/from "#input-regex-name" (ef/get-prop :value))
+                                        :desc (ef/from "#input-regex-desc" (ef/get-prop :value))
+                                        :regex (ef/from "#input-regex" (ef/get-prop :value))}}))))))))))
--- src/parva_pila_rex/handler.clj
+++ src/parva_pila_rex/handler.clj
@@ -5,17 +5,25 @@
             [ring.middleware.json :as ring-json]
             [ring.util.response :as rr]))
 
-(def regexes (atom [
-  {:regex (str #"ROFLROFL")}
-  {:regex (str #"^rofl$")}
-  {:regex (str #"\d{2}\.\d{2}\.\d{4}")}]))
+(def regexes (atom #{
+  {:name "Basic 1" :desc "A very simple regular expression" :regex (str #"ROFLROFL")}
+  {:name "Basic 2" :desc "A very simple regular expression" :regex (str #"^rofl$")}
+  {:name "German date" :desc "Tests if the given input denotes a German date" :regex (str #"\d{2}\.\d{2}\.\d{4}")}}))
+
+(defn update-regexes [regex action]
+  (when-not (get (:name regex) @regexes)
+    (swap! regexes #(action % regex))))
 
 (defroutes app-routes
   (GET "/regexes" []
+    (rr/response @regexes))
+  (POST "/regexes" request
+    (update-regexes (:body request) conj)
     (rr/response @regexes))
   (route/not-found "Not Found"))
 
 (def app
   (-> app-routes
     (ring-json/wrap-json-response)
-    (wrap-defaults site-defaults)))
+    (ring-json/wrap-json-body)
+    (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))

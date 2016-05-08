200e2f624dfc48b545d4f10ab030424fc64ad927
Torsten Scholz <doaschdn@googlemail.com>
2016-05-08 12:19:53 +0200

    Add config to frontend

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 (function () {
   'use strict';
   var app = angular.module('App', [
+    'Config',
     'Regexes'
   ]);
 })();
---
+++ src/components/Config/Config.prod.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+    var app = angular.module('Config');
+
+    app.constant('Config', {
+        BASE_PATH: ''
+    });
+})();
